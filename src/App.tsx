6e8f1e150f70bf7d0fe087f2a3ca104b29b696b0
carla.paiva <carla.paiva05@hotmail.com>
2025-12-15 21:49:33 -0300

    wip: initial commmit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>
+        Carla Paiva
+        <h2>Fullstack Software Developer</h2>
+      </h1>
+      <p>Em construção</p>
+    </div>
+  );
+}
+
+export default App;
